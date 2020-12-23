e784d0fe12c6cc3dfa271074ce703e0746444883
Erik Dubois <erik.dubois@gmail.com>
2020-12-23 15:49:50 +0100

    susuper + space

--- etc/skel/.config/arco-dwm/config.h
+++ etc/skel/.config/arco-dwm/config.h
@@ -116,7 +116,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ControlMask,			XK_comma,  cyclelayout,    {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
-	{ MODKEY,                       XK_space,  cyclelayout,    {0} },
+	{ MODKEY,                       XK_space,  cyclelayout,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
